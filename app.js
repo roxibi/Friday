57930a992b58d5cb10cb41ce09b9cb8b00f9d8f4
roxibi <106161599+roxibi@users.noreply.github.com>
2022-10-19 16:27:10 +0200

    adding iframe

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("it's working")
+const main = document.querySelector(".main")
+const iframe=`<iframe width="560" height="315" src="https://www.youtube.com/embed/1TewCPi92ro" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+function handleLoad(){
+    let day = new Date ()
+    if(day.getDay()===5){
+        main.innerHTML=iframe;
+    }
+
+}
+handleLoad()
