9b1c0a9fe9f7024acede0bf3cc6dc48356bd5451
roxibi <106161599+roxibi@users.noreply.github.com>
2022-10-19 17:46:17 +0200

    adding not friday gif

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const main = document.querySelector(".main")
 const img = document.querySelector(".gif");
+const imgNo = document.querySelector(".no-gif")
 const h1 =document.querySelector("h1");
 function handleLoad(){
     let day = new Date ()
-    if(day.getDay()===3){
+    if(day.getDay()===5){imgNo.style.display="none";
       return
     } else {img.style.display="none";
-    h1.innerText=""};
+    };
 
 }
 handleLoad()
